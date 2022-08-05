7a869c1ec187d78e3ab71a7f3ca46e621d476e69
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-08-05 17:46:46 -0300

    meetup items

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,43 @@
 <script>
   import Header from "./UI/Header.svelte";
+  import MeetupItem from "./Meetups/MeetupItem.svelte";
+
+  let meetups = [
+    {
+      id: Math.random(),
+      title: "This is a title",
+      subtitle: "This is a subtitle",
+      image: "https://source.unsplash.com/random/300x200",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, unde.",
+    },
+    {
+      id: Math.random(),
+      title: "This is a title2",
+      subtitle: "This is a subtitle2",
+      image: "https://source.unsplash.com/random/300x200",
+      content:
+        "2Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, unde.",
+    },
+    {
+      id: Math.random(),
+      title: "This is a title3",
+      subtitle: "This is a subtitle3",
+      image: "https://source.unsplash.com/random/300x200",
+      content:
+        "3Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, unde.",
+    },
+  ];
 </script>
 
 <Header />
+
+{#each meetups as meetup}
+  <MeetupItem
+    id={meetup.id}
+    title={meetup.title}
+    subtitle={meetup.subtitle}
+    image={meetup.image}
+    content={meetup.content}
+  />
+{/each}
---
+++ src/Meetups/MeetupItem.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let title = "";
+  export let subtitle = "";
+  export let image = "";
+  export let content = "";
+</script>
+
+<article>
+  <header>
+    <h1 class="">{title}</h1>
+    <h2>{subtitle}</h2>
+  </header>
+  <div>
+    <img src={image} class="image" />
+  </div>
+  <div class="content">
+    <p>Content</p>
+  </div>
+  <footer>
+    <button>Show Details</button>
+    <button>Favorite</button>
+  </footer>
+</article>
+
+<style>
+  article {
+    position: relative;
+    top: 5rem;
+    overflow: hidden;
+    z-index: -1;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    border: 1px solid #ccc;
+    margin: 10px;
+    padding: 15px;
+    box-shadow: 1px 1px 7px rgba(88, 86, 86, 0.26);
+  }
+</style>
